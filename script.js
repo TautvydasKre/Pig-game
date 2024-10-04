f11fb80c93046f5e87481dbc42c47c664f166ee5
TautvydasKre <144778291+TautvydasKre@users.noreply.github.com>
2024-10-04 15:20:19 +0300

    update

--- script.js
+++ script.js
@@ -24,17 +24,21 @@ btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6 + 1);
   console.log(dice);
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
   // 3. Check for rolled 1
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    document.getElementById(`currrent--${activePlayer}`).textContent =
+    document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
